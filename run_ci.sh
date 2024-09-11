2d95f0f1f67b84de672f597c71343934a9624c95
Alexander Lay <alexanderqlay@gmail.com>
2024-09-10 22:09:57 -0400

    ci: Move ci_venv creation into separate script
    Allows pre-commit to utilize same venv more easily
    
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- run_ci.sh
+++ run_ci.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
-# Run check if python installed
-check_python_installed() {
-    if ! command -v python3 &> /dev/null; then
-        echo "Python is NOT installed."
-        echo "Please install Python here: https://www.python.org/downloads/"
-        exit 1
-    fi
-}
-
-check_python_installed
-
 VENV_DIR="ci_venv"
 
 if [ ! -d $VENV_DIR ]; then
-    echo ===== CREATING CI_VENV =====
-
-    python3 -m venv "$VENV_DIR"
-    source "$VENV_DIR/bin/activate"
-
-    python3 -m pip install --upgrade pip
-    pip install pylint
-    pip install black
-    
-    echo ===== SETUP AND INSTALLATION OF PACKAGES COMPLETE =====
+    ./ci_venv.sh
 else
     echo ===== USING EXISTING CI_VENV =====
-    source "$VENV_DIR/bin/activate"
 fi
+
+source "$VENV_DIR/bin/activate"
 
 black . --exclude "ci_venv/"
 
