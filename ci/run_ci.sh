f89daf9640e607f6bcf39dd3c23495866dc4577d
Alexander Lay <alexanderqlay@gmail.com>
2024-09-20 22:18:02 -0400

    ci: Implement clang-format for Arduino code
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -19,3 +19,7 @@ echo ===== BLACK AUTOFORMATER COMPLETE =====
 find $path -name "*.py" -print0 | xargs -0 pylint --rcfile=ci/.pylintrc --disable=
 
 echo ===== PYLINT COMPLETE =====
+
+find $path -name '*.ino' -exec clang-format -i {} \;
+
+echo ===== CLANG-FORMAT COMPLETE =====
