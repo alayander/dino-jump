20c3ab32cd8f2edb704f9a9c1b9ac78784efe0ed
Alexander Lay <alexanderqlay@gmail.com>
2024-10-05 17:01:07 -0400

    ci: Move all handling of venv set-up/activation into ci_venv.sh
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- ci/run_ci.sh
+++ ci/run_ci.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-VENV_DIR="ci_venv"
-
 path=${1:-.}
 
-if [ ! -d $VENV_DIR ]; then
-    ./ci/ci_venv.sh
-else
-    echo ===== USING EXISTING CI_VENV =====
-fi
-
-os_name=$(uname)
-if [[ "$os_name" == "Linux" || "$os_name" == "Darwin" ]]; then
-    source "$VENV_DIR/bin/activate"
-elif [[ "$os_name" == "CYGWIN"* || "$os_name" == "MINGW"* ]]; then
-    source "$VENV_DIR/Scripts/activate"
-else
-    echo "Error reading back OS, complain to Alex to fix :("
-fi
+source ci/ci_venv.sh
 
 black $path --exclude "ci_venv/"
 
