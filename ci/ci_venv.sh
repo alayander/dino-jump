ae2bd5dab1f682acd2ff2027306e4c8c55f486d1
Alexander Lay <alexanderqlay@gmail.com>
2024-10-05 17:01:07 -0400

    ci: Move all handling of venv set-up/activation into ci_venv.sh
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

--- ci/ci_venv.sh
+++ ci/ci_venv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VENV_DIR="ci_venv"
+PACKAGES=("pylint" "black" "clang-format")
 
 # Run check if python installed
 check_python_installed() {
@@ -11,25 +12,45 @@ check_python_installed() {
     fi
 }
 
+check_packages_installed() {
+    for pkg in "${PACKAGES[@]}"; do
+        pip show "$pkg" > /dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            pip install "$pkg"
+        fi
+    done
+}
+
+activate_venv() {
+    os_name=$(uname)
+    if [[ "$os_name" == "Linux" || "$os_name" == "Darwin" ]]; then
+        source "$VENV_DIR/bin/activate"
+    elif [[ "$os_name" == "CYGWIN"* || "$os_name" == "MINGW"* ]]; then
+        source "$VENV_DIR/Scripts/activate"
+    else
+        echo "Error reading back OS, complain to Alex to fix :("
+        exit 1
+    fi
+}
+
 check_python_installed
 
-echo ===== CREATING CI_VENV =====
+if [ ! -d $VENV_DIR ]; then
+    echo ===== CREATING CI_VENV =====
 
-python3 -m venv "$VENV_DIR"
+    python3 -m venv "$VENV_DIR"
 
-os_name=$(uname)
-if [[ "$os_name" == "Linux" || "$os_name" == "Darwin" ]]; then
-    source "$VENV_DIR/bin/activate"
-elif [[ "$os_name" == "CYGWIN"* || "$os_name" == "MINGW"* ]]; then
-    source "$VENV_DIR/Scripts/activate"
+    activate_venv
+
+    python3 -m pip install --upgrade pip
+    pip install pylint
+    pip install black
+    pip install clang-format
+
+    echo ===== SETUP AND INSTALLATION OF PACKAGES COMPLETE =====
 else
-    echo "Error reading back OS, complain to Alex to fix :("
-    exit 1
+    echo ===== USING EXISTING CI_VENV =====
+
+    activate_venv
+    check_packages_installed
 fi
-
-python3 -m pip install --upgrade pip
-pip install pylint
-pip install black
-pip install clang-format
-
-echo ===== SETUP AND INSTALLATION OF PACKAGES COMPLETE =====
