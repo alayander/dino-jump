63ad49ec9bca56681c73ca6b38d025b4c9fa88b7
Brian Tsoi <brian.s.tsoi@gmail.com>
2024-12-25 17:37:01 -0500

    game: Add C++ emulator example
    Signed-off-by: Brian Tsoi <brian.s.tsoi@gmail.com>

---
+++ game/cpp/emulator_example/include/frames.hpp
@@ -0,0 +1,11 @@
+#ifndef FRAMES_HPP
+#define FRAMES_HPP
+
+// In the actual game, SCREEN_HEIGHT and SCREEN_WIDTH should probably be in
+// something like game_constants.hpp
+const int SCREEN_HEIGHT = 48;
+const int SCREEN_WIDTH = 150;
+const int FRAME_NUM = 45;
+extern const int FRAMES[FRAME_NUM][SCREEN_HEIGHT][SCREEN_WIDTH];
+
+#endif // FRAMES_HPP
---
+++ game/cpp/emulator_example/src/game.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "emulator.hpp"
+#include "frames.hpp"
+
+int main() {
+  init_terminal();
+
+  // The pause variable is purely for showing that the spacebar can be used for
+  // input. It is not needed for the actual game
+  bool pause = false;
+  int f = 0;
+  while (true) {
+    char key = read_key();
+    if (key == 'q') {
+      exit(0);
+    }
+    if (key == ' ') {
+      pause = !pause;
+    }
+
+    if (!pause) {
+      f = (f + 1) % FRAME_NUM;
+    }
+    print_frame(FRAMES[f]);
+    usleep(10000);
+  }
+  return 0;
+}
