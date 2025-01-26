beb02cfa14a26cf8917aabb31c7284b6a98f708b
Alexander Lay <alexanderqlay@gmail.com>
2025-01-25 23:14:34 -0500

    game: Add initial C++ implementation of game code
    This implementation includes a terminal_display.cpp to emulate the output of the game, which will be replaced by the hologram fan
    
    Signed-off-by: Alexander Lay <alexanderqlay@gmail.com>

---
+++ game/cpp/production/include/terminal_display.hpp
@@ -0,0 +1,20 @@
+#ifndef TERMINAL_DISPLAY_HPP
+#define TERMINAL_DISPLAY_HPP
+
+#include "constants.hpp"
+
+#include <array>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+void init_terminal();
+void reset_terminal();
+char read_key();
+void print_frame(const std::array<std::array<int, MAX_X>, MAX_Y> &frame);
+
+#endif // TERMINAL_DISPLAY_HPP
