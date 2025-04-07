245aded262e239a59bb0a00362ed1ef7b9294bc9
Alexander Lay <alexanderqlay@gmail.com>
2025-04-06 23:00:11 -0400

    Add states for ESP to ESP communications

--- dino_jump/dino_jump.ino
+++ dino_jump/dino_jump.ino
@@ -3,29 +3,43 @@
 #include "hologram_fan.hpp"
 #include "title_frame.hpp"
 
-#define FAN_PIN D6
+/* Display Pins */
 #define RESET_PIN D7
-#define IR_PIN D8
+#define PROXIMITY_PIN D8
 #define BEAM_BREAK_PIN D9
-#define JUMP_PIN D10
-#define DUCK_PIN D11
-#define START_PIN D12
+
+/* Base ESP32 Pins */
+#define BASE_INPUT0_PIN D10
+#define BASE_INPUT1_PIN D11
+#define BASE_OUTPUT_PIN D12
+
+
+enum State {
+  IDLE,
+  TITLE,
+  GAME,
+  DEATH,
+};
+
 
 Game game;
 HologramFan display;
+State currentState = IDLE;
+bool advance = false;
 bool jumped = false;
+bool timedout = false;
+
 
 void setup() {
   Wire.begin();
   Wire.setClock(800000);
 
-  pinMode(IR_PIN, INPUT);
+  pinMode(PROXIMITY_PIN, INPUT);
   pinMode(BEAM_BREAK_PIN, INPUT_PULLUP);
-  pinMode(START_PIN, INPUT_PULLUP);
-  pinMode(JUMP_PIN, INPUT);
-  pinMode(DUCK_PIN, INPUT);
 
-  pinMode(FAN_PIN, OUTPUT);
+  pinMode(BASE_INPUT0_PIN, INPUT);
+  pinMode(BASE_INPUT1_PIN, INPUT);
+  pinMode(BASE_OUTPUT_PIN, OUTPUT);
 
   pinMode(RESET_PIN, OUTPUT);
   digitalWrite(RESET_PIN, LOW);
@@ -34,42 +48,74 @@ void setup() {
 
   display.begin();
 
-  attachInterrupt(digitalPinToInterrupt(JUMP_PIN), handleJump, RISING);
+  attachInterrupt(digitalPinToInterrupt(BASE_INPUT1_PIN), handle_jump, RISING);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  title_loop();
+  switch (currentState) {
+    case IDLE:
+      idle_loop();
+      break;
+    case TITLE: 
+      title_loop();
+      break;
+    case GAME:
+      game_loop();
+      break;
+    case DEATH:
+      death_loop();
+      break;
+  }
+}
+
+void idle_loop() {
+  attachInterrupt(digitalPinToInterrupt(BASE_INPUT0_PIN), handle_state_input, RISING);
+  advance = false;
   
-  game_loop();
+  while (!advance) {
+    Serial.println("Idle");
+  }
+  currentState = TITLE;
 }
 
 void title_loop() {
-  while(digitalRead(START_PIN) == HIGH) {
-    delay(1);
-  }
-  digitalWrite(FAN_PIN, LOW);
-  digitalWrite(FAN_PIN, HIGH);
-  delay(1000);
+  advance = false;
+  timedout = false;
   
-  while (digitalRead(START_PIN) == HIGH) {
-    if (digitalRead(IR_PIN) == LOW) {
+  while (!advance && !timedout) {
+    Serial.println("Title");
+    if (jumped) {
+      jumped = false;
+      timedout = true;
+    }
+    
+    if (digitalRead(PROXIMITY_PIN) == LOW) {
       display.flash_frame(TITLE_FRAME, digitalRead(BEAM_BREAK_PIN) == HIGH ? 1 : 0);
     }
     delay(1);
   }
+
+  if (timedout) {
+    currentState = IDLE;
+  } else {
+    currentState = GAME;
+  }
 }
 
 void game_loop() {
-  while (true) {
-    if (digitalRead(IR_PIN) == LOW) {
-      // Normally flashing a frame takes ~0.1s
+  detachInterrupt(digitalPinToInterrupt(BASE_INPUT0_PIN));
+  
+  while (!game.get_collision()) {
+    Serial.println("Game");
+    if (digitalRead(PROXIMITY_PIN) == LOW) {
       display.flash_frame(game.get_frame(), digitalRead(BEAM_BREAK_PIN) == HIGH ? 1 : 0);
   
-      // Updating game states take ~0.4ms
       if (jumped) {
         game.input(Input_State::JUMP);
         jumped = false;
-      } else if (digitalRead(DUCK_PIN) == HIGH) {
+      } else if (digitalRead(BASE_INPUT0_PIN) == HIGH) {
         game.input(Input_State::DUCK);
       } else {
         game.input(Input_State::NEUTRAL);
@@ -79,8 +125,27 @@ void game_loop() {
     }
     delay(1);
   }
+  currentState = DEATH;
 }
 
-void handleJump() {
+void death_loop() {
+  unsigned long death_screen_start = millis();
+  while (millis() - death_screen_start < 10000) {
+    Serial.println("Death");
+    display.flash_frame(game.get_frame(), digitalRead(BEAM_BREAK_PIN) == HIGH ? 1 : 0);
+  }
+
+  // Communicate game over to Base
+  digitalWrite(BASE_OUTPUT_PIN, HIGH);
+  digitalWrite(BASE_OUTPUT_PIN, LOW);
+
+  currentState = TITLE;
+}
+
+void handle_state_input() {
+  advance = true;
+}
+
+void handle_jump() {
   jumped = true;
 }
